c1342b4a4da0a40a3a7830e97aac87bd26a57731
divyanshu <divyanshu@accelaero.com>
2020-08-17 21:48:40 +0530

    Created spring boot project

---
+++ src/main/java/com/mediaocean/tournament_scheduling/TournamentSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.mediaocean.tournament_scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediaocean/tournament_scheduling/TournamentSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediaocean.tournament_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
